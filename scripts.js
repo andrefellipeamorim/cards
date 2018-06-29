d7e77456b8fa157a745c0e64835969337b9a03d6
andrefellipeamorim <38409430+andrefellipeamorim@users.noreply.github.com>
2018-06-29 14:34:12 -0300

    comentarios

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+	//Funções referentes ao Deck principal. ->
+
 	$('.card').mouseover(function(){
 		var id = this.id;		
 		$('#'+id).addClass('cardtop');		
@@ -9,13 +12,18 @@ $(document).ready(function(){
 		$('#'+id).removeClass('cardtop');
 	});
 
+	/*Esta função não é necessária do meu ponto de vista, 
+	fiz apenas para que o lay-out fique sempre como pedido na imagem -> */
+
 	$('.fundo').mouseover(function(){
 		$('#card2').addClass('cardtop');
 	});
 
 	$('.fundo').mouseout(function(){
 		$('#card2').removeClass('cardtop');
-	});	
+	});
+
+	//Funções referentes ao Deck horizontal. ->	
 
 	$('#card-sm-1').mouseover(function(){
 		$('#card-sm-2').addClass('cardmid');
